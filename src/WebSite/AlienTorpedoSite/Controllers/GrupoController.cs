c6c87587b04189b82a9b7b7faa08ae9b6203e6f3
Unknown <massami.shim@gmail.com>
2017-09-22 17:55:35 -0300

    Auto stash before merge of "Feature" and "N0gueiraV/Development"

--- src/WebSite/AlienTorpedoSite/Controllers/GrupoController.cs
+++ src/WebSite/AlienTorpedoSite/Controllers/GrupoController.cs
@@ -1,4 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using AlienTorpedoSite.Models.Grupo;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Microsoft.AspNetCore.Http;
+using AlienTorpedoSite.ViewModels.Grupo;
+using System;
 
 namespace AlienTorpedoSite.Controllers
 {
@@ -20,6 +26,60 @@ namespace AlienTorpedoSite.Controllers
         {
             ViewData["Title"] = "Atrelar Evento";
             return View();
+        }
+
+        public IActionResult btnSorteio(GrupoViewModel grupoViewModel)
+        {
+            GrupoEvento grupoEvento = new GrupoEvento
+            {
+                IdGrupoEvento = 18
+            };
+
+            //Chamando API para cadastrar o usuário na base 
+            using (HttpClient client = new HttpClient())
+            {
+                //Setando endereço da API
+                client.BaseAddress = new System.Uri("http://localhost:53462/api/sorteio");
+                //Limpando header
+                client.DefaultRequestHeaders.Accept.Clear();
+                //Adicionando um novo header do tipo JSON
+                client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/json"));
+
+                //Transformando obj Usuario em uma string
+                string stringData = JsonConvert.SerializeObject(grupoEvento);
+
+                //Transformando string em um arquivo do tipo JSON
+                var contentData = new StringContent(stringData, System.Text.Encoding.UTF8, "application/json");
+
+                //Chamando API passando o arquivo JSON
+                HttpResponseMessage response = client.PostAsync("http://localhost:53462/api/sorteio", contentData).Result;
+
+                //Passando retorno da API para uma string
+                string retorno = response.Content.ReadAsStringAsync().Result;
+
+                //Fragmentando retorno do arquivo json 
+                dynamic resultado = JsonConvert.DeserializeObject(retorno);
+
+                //Enviando retorno para a tela
+
+                ViewBag.grupoEvento = resultado.idGrupoEvento;
+                ViewBag.cdGrupo = resultado[0].cdGrupo.Value;
+                ViewBag.dtEvento = resultado[0].dtEvento.Value;
+                ViewBag.nmEvento = resultado[0].nmEvento.Value;
+                ViewBag.nmEndereco = resultado[0].nmEndereco.Value;
+                ViewBag.vlEvento = resultado[0].vlEvento.Value;
+
+                GrupoViewModel viewModel = new GrupoViewModel
+                {
+                    IdGrupoEvento = resultado.idGrupoEvento,
+                    cdGrupo = resultado.cdGrupo,
+                    dtEvento = resultado.dtEvento,
+                    nmEvento = resultado.nmEvento,
+                    nmEndereco = resultado.nmEndereco,
+                    vlEvento = resultado.vlEvento
+                };
+                return View(viewModel);
+            }
         }
     }
 }
