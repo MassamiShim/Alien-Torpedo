068cae83cf779813dfcb490c155316b8587a7274
Unknown <massami.shim@gmail.com>
2020-09-28 19:21:27 -0300

    Atv: Criação da lógica para realização do sorteio
    Ajustado a lógica do sorteio, e criado o processo para gerar todos os sorteios pendentes.

--- src/WebAPI/AlienTorpedoAPI/Controllers/SorteioController.cs
+++ src/WebAPI/AlienTorpedoAPI/Controllers/SorteioController.cs
@@ -1,12 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
 using AlienTorpedoAPI.Classes;
 using AlienTorpedoAPI.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using System.Collections.Generic;
 
 namespace AlienTorpedoAPI.Controllers
 {
@@ -25,18 +21,31 @@ namespace AlienTorpedoAPI.Controllers
 
         // POST: api/Sorteio/Sortear
         [HttpPost]
-        public IActionResult Sortear([FromBody]GrupoEvento grupoEvento)
+        public IActionResult Sortear([FromBody] GrupoEvento grupoEvento)
         {
             Sorteio sorteio = new Sorteio(_configuration);
             int cdEvento = sorteio.GeraSorteio(grupoEvento, _dbcontext);
             var result = new List<GrupoEventoViewModel>();
 
-            if (cdEvento == 0)           
-                return Json(new { cdretorno = 1, mensagem = "O evento selecionado n�o est� atrelado a este grupo!", data = result });            
+            if (cdEvento == 0)
+                return Json(new { cdretorno = 1, mensagem = "O evento selecionado não está atrelado a este grupo!", data = result });
 
             result = sorteio.BuscaSorteio(_dbcontext, grupoEvento);
 
             return Json(new { cdretorno = 0, mensagem = "Sucesso!", data = result });
+        }
+
+        // POST: api/Sorteio/SortearTodos
+        [HttpPost]
+        public IActionResult SortearTodos([FromBody] Grupo grupo)
+        {
+            Sorteio sorteio = new Sorteio(_configuration);
+            int resultado = sorteio.GeraSorteioTodos(grupo, _dbcontext);
+
+            if (resultado == 0)
+                return Json(new { cdretorno = 1, mensagem = "Eventos não foram sorteados!" });
+
+            return Json(new { cdretorno = 0, mensagem = "Sucesso!" });
         }
     }
 }
